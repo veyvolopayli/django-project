6d893ae8437c8c9fb17e28b987cf8d7960df5660
Ilya <60238433+veyvolopayli@users.noreply.github.com>
2025-03-28 08:06:11 +0000

    changes

--- django/polls/views.py
+++ django/polls/views.py
@@ -9,41 +9,6 @@ from .serializers import QuestionSerializer, ChoiceSerializer, UserProfileSerial
 from .models import Question, Choice, UserProfile
 
 
-def index(request):
-    latest_questions = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_questions': latest_questions}
-    return render(request, 'app/index.html', context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'app/detail.html', {'question': question})
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return render(request, 'app/detail.html', {
-            'question': question,
-            'error_message': "Вы не выбрали ответ."
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse('results', args=(question.id,)))
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'app/results.html', {'question': question})
-
-
-def user_profile(request, username):
-    user = get_object_or_404(UserProfile, username=username)
-    return render(request, 'app/profile.html', {'user': user})
-
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
