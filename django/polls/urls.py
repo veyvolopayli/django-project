27ad3b0c5bb586d665da2b008dc08d8b3b6ab99f
Ilya <60238433+veyvolopayli@users.noreply.github.com>
2025-03-28 08:14:13 +0000

    last changes

--- django/polls/urls.py
+++ django/polls/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import QuestionViewSet, ChoiceViewSet, UserProfileViewSet, index, detail, vote, results, user_profile
+from .views import QuestionViewSet, ChoiceViewSet, UserProfileViewSet
 
 router = DefaultRouter()
 router.register(r'questions', QuestionViewSet)
@@ -8,10 +8,5 @@ router.register(r'choices', ChoiceViewSet)
 router.register(r'users', UserProfileViewSet)
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('<int:question_id>/', detail, name='detail'),
-    path('<int:question_id>/vote/', vote, name='vote'),
-    path('<int:question_id>/results/', results, name='results'),
-    path('profile/<str:username>/', user_profile, name='user_profile'),
-    path('api/', include(router.urls)),  # Подключаем маршруты API
+    path('api/', include(router.urls))
 ]
